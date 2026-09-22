68c8363934c67d5801c25b7ab96032463a269f2f
Mkhalid2300 <m.khalid2300@nu.edu.eg>
2026-09-22 14:34:59 +0300

    Add course_details_bootstrap.html template

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'question_grade', 'course')
+    list_filter = ['course']
+    search_fields = ['question_text']
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline, QuestionInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course', 'mode', 'rating']
+
 
 # Register your models here.
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Enrollment, EnrollmentAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
